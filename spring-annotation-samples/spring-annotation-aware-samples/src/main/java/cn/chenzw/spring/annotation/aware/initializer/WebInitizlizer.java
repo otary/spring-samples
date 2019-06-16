4351da9cd63ae23a771dedcf636b0cad55a1fa2e
chenzw <656469722@qq.com>
2019-06-17 00:15:02 +0800

    initialization

--- spring-annotation-samples/spring-annotation-aware-samples/src/main/java/cn/chenzw/spring/annotation/aware/initializer/WebInitizlizer.java
+++ spring-annotation-samples/spring-annotation-aware-samples/src/main/java/cn/chenzw/spring/annotation/aware/initializer/WebInitizlizer.java
@@ -1,9 +1,14 @@
 package cn.chenzw.spring.annotation.aware.initializer;
 
+import cn.chenzw.spring.annotation.aware.config.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
 
 /**
  * Servlet 3.0+
@@ -14,6 +19,15 @@ public class WebInitizlizer implements WebApplicationInitializer {
 
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-        System.out.println("-----------------------");
+        AnnotationConfigWebApplicationContext wac = new AnnotationConfigWebApplicationContext();
+        wac.register(AppConfig.class);
+        wac.setServletContext(servletContext);
+
+        ServletRegistration.Dynamic servletRegistration = servletContext.addServlet("dispatcher", new DispatcherServlet(wac));
+        servletRegistration.addMapping("/");
+        servletRegistration.setLoadOnStartup(1);
+
+        System.out.println("---------startup---------------");
+
     }
 }
