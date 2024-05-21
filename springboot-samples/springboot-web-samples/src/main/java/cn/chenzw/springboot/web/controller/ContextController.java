3726a9038f369224f959d257d4c2115d715b0493
chenzw2 <ffchenzw@chinatelecom.cn>
2024-05-21 15:48:16 +0800

    update

--- springboot-samples/springboot-web-samples/src/main/java/cn/chenzw/springboot/web/controller/ContextController.java
+++ springboot-samples/springboot-web-samples/src/main/java/cn/chenzw/springboot/web/controller/ContextController.java
@@ -1,10 +1,14 @@
 package cn.chenzw.springboot.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.system.ApplicationHome;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ui.context.Theme;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +22,13 @@ import org.springframework.web.servlet.support.RequestContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -116,4 +127,23 @@ public class ContextController {
         });
     }
 
+    @GetMapping("/jarPath")
+    public String getJarPath() {
+        log.info("java.class.path => {}", new File(System.getProperty("java.class.path")).getParent()); // => null
+        log.info("codeSourceLocation => {}", new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()).getParent()); // => file:\...\springboot-web-samples-0.0.1-SNAPSHOT.jar!\BOOT-INF
+        log.info("thread resource path => {}", Thread.currentThread().getContextClassLoader().getResource("").getPath()); // =>  file:/.../springboot-web-samples-0.0.1-SNAPSHOT.jar!/BOOT-INF/classes!/
+
+        log.info("user.dir => {}", System.getProperty("user.dir")); // => jar包所在目录
+        ApplicationHome ah = new ApplicationHome(getClass());
+        return ah.getSource().getParentFile().getAbsolutePath(); // => jar包所在目录
+    }
+
+    @GetMapping("/classpath/file")
+    public String getClassPathResourceContent() throws IOException {
+        ClassPathResource classPathResource = new ClassPathResource("a.txt");
+        log.info("path => {}", classPathResource.getPath());
+        log.info("exists => {}", classPathResource.exists());
+
+        return IOUtils.toString(classPathResource.getInputStream(), Charset.defaultCharset());
+    }
 }
